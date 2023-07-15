58c5e41ce54209a9d377bd279e73b805155ecb5d
cyp-seculyze <avicocyprien@yahoo.com>
2023-07-15 22:14:33 +0200

    oui

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,14 @@
+//use std::fs;
 use walkdir::WalkDir;
 
 fn main() {
-    let mut count=0;
-    for _ in WalkDir::new("/").into_iter().filter_map(|file| file.ok()) {
-        //println!("{}", file.path().display());
-        count+=1;
+    let mut count = 0;
+    for entry in WalkDir::new("/").into_iter().filter_map(|entry| entry.ok()) {
+        let path = entry.path();
+        if path.is_file() {
+            //println!("{}", path.display());
+            count+=1;
+        }
     }
-    println!("{}", count)
+    println!("{}", count);
 }
