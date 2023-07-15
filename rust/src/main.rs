84c3a1a5ce5f81f5cd3e44b6a0af5b69d1646cfd
cyp-seculyze <avicocyprien@yahoo.com>
2023-07-15 22:02:14 +0200

    Update main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,49 +1,10 @@
-use std::fs::File;
-use std::io::{self, Write};
-use std::time::Instant;
 use walkdir::WalkDir;
 
-fn list_files_recursive(directory: &str, output_file: &str, log_file: &str) -> io::Result<()> {
-    let start_time = Instant::now();
-
-    let file = File::create(output_file)?;
-    let mut writer = io::BufWriter::new(file);
-
-    let log_file = File::create(log_file)?;
-    let mut log_writer = io::BufWriter::new(log_file);
-
-    for entry in WalkDir::new(directory).follow_links(true) {
-        match entry {
-            Ok(entry) => {
-                if entry.file_type().is_file() {
-                    if let Some(path) = entry.path().to_str() {
-                        writeln!(writer, "{}", path)?;
-                    }
-                }
-            }
-            Err(error) => {
-                if let Some(path) = error.path() {
-                    if let Some(path_str) = path.to_str() {
-                        writeln!(log_writer, "Error accessing file: {}", path_str)?;
-                    }
-                }
-            }
-        }
-    }
-
-    let elapsed_time = start_time.elapsed().as_secs_f64();
-    writeln!(log_writer, "Execution time: {:.2} seconds", elapsed_time)?;
-
-    Ok(())
-}
-
 fn main() {
-    let directory = "/home";
-    let output_file = "files_rust.txt";
-    let log_file = "list_files_log.log";
-
-    match list_files_recursive(directory, output_file, log_file) {
-        Ok(_) => println!("List of regular files written to {}.", output_file),
-        Err(error) => eprintln!("Error: {}", error),
+    let mut count=0;
+    for _ in WalkDir::new("/").into_iter().filter_map(|file| file.ok()) {
+        //println!("{}", file.path().display());
+        count+=1;
     }
+    println!("{}", count)
 }
