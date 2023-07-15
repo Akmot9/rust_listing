1afced64df0b3ef24b5993cb5cfd80c8a11e45ab
cyp-seculyze <avicocyprien@yahoo.com>
2023-07-15 21:56:09 +0200

    liste type take args path

--- files_type.py
+++ files_type.py
@@ -1,5 +1,7 @@
 import os
 import stat
+import sys
+
 
 def count_file_types(file_path):
     regular_files = 0
@@ -7,8 +9,8 @@ def count_file_types(file_path):
     symbolic_links = 0
     pipes = 0
     sockets = 0
-    unknown =0
-    
+    unknown = 0
+
     with open(file_path, 'r') as file:
         for line in file:
             line = line.strip()
@@ -31,15 +33,15 @@ def count_file_types(file_path):
 
             except FileNotFoundError:
                 print(f"File or directory not found: {line}")
-                unknown+=1
+                unknown += 1
             except OSError as e:
                 if e.errno == 40:
-                    unknown+=1
+                    unknown += 1
                     print(f"Too many levels of symbolic links: {line}")
                 else:
-                    unknown+=1
+                    unknown += 1
                     print(f"Error accessing file or directory: {line}, {e}")
-    
+
     result = {
         'regular_files': regular_files,
         'directories': directories,
@@ -47,30 +49,37 @@ def count_file_types(file_path):
         'pipes': pipes,
         'sockets': sockets,
         'unknown': unknown,
-        'total':unknown+sockets+pipes+symbolic_links+
-        directories+regular_files
+        'total': unknown + sockets + pipes + symbolic_links +
+                 directories + regular_files
     }
-    
+
     return result
+
 
 def count_files_per_directory(file_path):
     result = {}
-    
+
     with open(file_path, 'r') as file:
         for line in file:
             line = line.strip()
             if not line:
                 continue
-            
+
             directory = line.split('/')[1]
             if directory in result:
                 result[directory] += 1
             else:
                 result[directory] = 1
-    
+
     return result
 
 
-path_files = "/home/akmot/Documents/GitHub/rust_listing/bash/list_flies/list_files.txt"
-liste_types_files = count_file_types(path_files)
-print(liste_types_files)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Please provide the file path as an argument.")
+    else:
+        file_path = sys.argv[1]
+        liste_types_files = count_file_types(file_path)
+        liste_dir_files = count_files_per_directory(file_path)
+        print(liste_types_files)
+        print(liste_dir_files)
